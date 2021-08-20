906c18e05b1cb13a8eb81d228843b8bc0084d537
fcventura02 <filipecvr4@gmail.com>
2021-08-20 17:38:39 -0300

    Updated page info time

--- src/scripts/infoTeam.js
+++ src/scripts/infoTeam.js
@@ -11,32 +11,53 @@ const teamDescription = document.getElementById("team-description");
 const teamTeamGalery = document.getElementById("team-galery");
 const teamTeamNetwork = document.getElementById("team-network");
 
+idTeam === null
+  ? notIdSerach(
+      ` 
+    <p><strong>Você não selecionou um time!</strong>
+    <br>
+    Você sera redirecionado para a pagina principal.</p>
+  `
+    )
+  : searchInfoTeam();
+
+function notIdSerach(info) {
+  bootbox.alert(info, () => {
+    window.location.href = "./index.html";
+  });
+}
+
 async function searchInfoTeam() {
-  const respAxios = await apiSearchTeam();
-  const {
-    strTeamBadge,
-    strTeamJersey,
-    strTeamLogo,
-    strTeamFanart1,
-    strTeamFanart2,
-    strTeamFanart3,
-    strTeamFanart4,
-    strTeamBanner,
-  } = respAxios.teams[0];
-  createHeaderElementFromTeamInformation(respAxios.teams[0]);
-  createElementWithTeamStadiumInformation(respAxios.teams[0]);
-  createElementWithTeamDescription(respAxios.teams[0]);
-  createElementWithGallery(
-    strTeamBadge,
-    strTeamJersey,
-    strTeamLogo,
-    strTeamFanart1,
-    strTeamFanart2,
-    strTeamFanart3,
-    strTeamFanart4,
-    strTeamBanner
-  );
-  createElementWithSocialNetworks(respAxios.teams[0]);
+  try {
+    const respAxios = await apiSearchTeam();
+    const {
+      strTeamBadge,
+      strTeamJersey,
+      strTeamLogo,
+      strTeamFanart1,
+      strTeamFanart2,
+      strTeamFanart3,
+      strTeamFanart4,
+      strTeamBanner,
+    } = respAxios.teams[0];
+    createHeaderElementFromTeamInformation(respAxios.teams[0]);
+    createElementWithTeamStadiumInformation(respAxios.teams[0]);
+    createElementWithTeamDescription(respAxios.teams[0]);
+    createElementWithGallery(
+      strTeamBadge,
+      strTeamJersey,
+      strTeamLogo,
+      strTeamFanart1,
+      strTeamFanart2,
+      strTeamFanart3,
+      strTeamFanart4,
+      strTeamBanner
+    );
+    createElementWithSocialNetworks(respAxios.teams[0]);
+  } catch (error) {
+    console.error(error);
+    notIdSerach(`<h3>Não foi possivel acessar o time informado!</h3>`);
+  }
 }
 
 function apiSearchTeam() {
@@ -74,14 +95,24 @@ function createElementWithTeamStadiumInformation({
   const h3 = document.createElement("h3");
   const span = document.createElement("span");
   const p = document.createElement("p");
-  h3.innerText = `${strStadium}`;
-  span.innerText = `capacidade - ${intStadiumCapacity}`;
-  p.innerText = `${strStadiumDescription}`;
-  if (strStadiumThumb)
-    teamStadiumImg.innerHTML = `<img src="${strStadiumThumb}" alt="estadio ${strStadium}">`;
-  teamStadiumInfo.appendChild(h3);
-  teamStadiumInfo.appendChild(span);
-  teamStadiumInfo.appendChild(p);
+  console.log(strStadium || intStadiumCapacity || strStadiumDescription)
+  if (strStadium || intStadiumCapacity || strStadiumDescription) {
+    h3.innerText =
+      strStadium === null ? "Sem informação do nome estádio" : `${strStadium}`;
+    span.innerText =
+      intStadiumCapacity === null
+        ? "Sem informação da capacidade do estádio"
+        : `capacidade - ${intStadiumCapacity}`;
+    p.innerText =
+      strStadiumDescription === null
+        ? "Sem Descrição do estádio"
+        : `${strStadiumDescription}`;
+    if (strStadiumThumb)
+      teamStadiumImg.innerHTML = `<img src="${strStadiumThumb}" alt="estadio ${strStadium}">`;
+    teamStadiumInfo.appendChild(h3);
+    teamStadiumInfo.appendChild(span);
+    teamStadiumInfo.appendChild(p);
+  }
 }
 
 function createElementWithTeamDescription({
@@ -95,16 +126,11 @@ function createElementWithTeamDescription({
 }
 
 function createElementWithGallery(...props) {
-  console.log(props);
-  let first = true;
   for (const iterator of props) {
     if (iterator) {
       const img = document.createElement("img");
       const div = document.createElement("div");
-      first
-        ? div.setAttribute("class", "carousel-item active")
-        : div.setAttribute("class", "carousel-item");
-      first = false;
+      div.setAttribute("class", "carousel-item");
       img.setAttribute("src", iterator);
       div.appendChild(img);
       teamTeamGalery.appendChild(div);
@@ -124,7 +150,16 @@ function createElementWithSocialNetworks({
   strInstagram &&
     teamTeamNetwork.appendChild(createLink(strInstagram, "Instagram"));
   strTwitter && teamTeamNetwork.appendChild(createLink(strTwitter, "twitter"));
-
+  if (
+    strYoutube === "" &&
+    strFacebook === "" &&
+    strInstagram === "" &&
+    strTwitter === ""
+  ) {
+    const p = document.createElement("p");
+    p.innerText = "Esse time não possui redes sociais";
+    teamTeamNetwork.appendChild(p);
+  }
   function createLink(url, classe) {
     const link = document.createElement("a");
     link.setAttribute("href", `https://${url}`);
@@ -141,5 +176,3 @@ function createElementWithSocialNetworks({
     return link;
   }
 }
-
-searchInfoTeam();
