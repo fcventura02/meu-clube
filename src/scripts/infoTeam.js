d29447ccfed2a404ec333543399e02960743b354
fcventura02 <filipecvr4@gmail.com>
2021-08-20 00:18:17 -0300

    added features

---
+++ src/scripts/infoTeam.js
@@ -0,0 +1,145 @@
+import api from "./api.js";
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const idTeam = urlParams.get("id");
+const teamLogo = document.getElementById("team-logo");
+const teamInfo = document.getElementById("team-info");
+const teamStadiumInfo = document.getElementById("team-stadium-info");
+const teamStadiumImg = document.getElementById("team-stadium-img");
+const teamDescription = document.getElementById("team-description");
+const teamTeamGalery = document.getElementById("team-galery");
+const teamTeamNetwork = document.getElementById("team-network");
+
+async function searchInfoTeam() {
+  const respAxios = await apiSearchTeam();
+  const {
+    strTeamBadge,
+    strTeamJersey,
+    strTeamLogo,
+    strTeamFanart1,
+    strTeamFanart2,
+    strTeamFanart3,
+    strTeamFanart4,
+    strTeamBanner,
+  } = respAxios.teams[0];
+  createHeaderElementFromTeamInformation(respAxios.teams[0]);
+  createElementWithTeamStadiumInformation(respAxios.teams[0]);
+  createElementWithTeamDescription(respAxios.teams[0]);
+  createElementWithGallery(
+    strTeamBadge,
+    strTeamJersey,
+    strTeamLogo,
+    strTeamFanart1,
+    strTeamFanart2,
+    strTeamFanart3,
+    strTeamFanart4,
+    strTeamBanner
+  );
+  createElementWithSocialNetworks(respAxios.teams[0]);
+}
+
+function apiSearchTeam() {
+  return api.get(`lookupteam.php?id=${idTeam}`).then((response) => {
+    return response.data;
+  });
+}
+
+function createHeaderElementFromTeamInformation({
+  strTeamBadge,
+  strTeam,
+  strStadiumLocation,
+  strCountry,
+  strLeague,
+  strSport,
+}) {
+  const h1 = document.createElement("h1");
+  const span = document.createElement("span");
+  const p = document.createElement("p");
+  h1.innerHTML = `<h1>${strTeam}</h1>`;
+  span.innerHTML = `<span>${strStadiumLocation} - ${strCountry}</span> <br>`;
+  p.innerHTML = `<p> ${strLeague} - ${strSport}</p>`;
+  teamLogo.innerHTML = `<img src="${strTeamBadge}" alt="logo ${strTeam}">`;
+  teamInfo.appendChild(h1);
+  teamInfo.appendChild(span);
+  teamInfo.appendChild(p);
+}
+
+function createElementWithTeamStadiumInformation({
+  strStadium,
+  strStadiumDescription,
+  strStadiumThumb,
+  intStadiumCapacity,
+}) {
+  const h3 = document.createElement("h3");
+  const span = document.createElement("span");
+  const p = document.createElement("p");
+  h3.innerText = `${strStadium}`;
+  span.innerText = `capacidade - ${intStadiumCapacity}`;
+  p.innerText = `${strStadiumDescription}`;
+  if (strStadiumThumb)
+    teamStadiumImg.innerHTML = `<img src="${strStadiumThumb}" alt="estadio ${strStadium}">`;
+  teamStadiumInfo.appendChild(h3);
+  teamStadiumInfo.appendChild(span);
+  teamStadiumInfo.appendChild(p);
+}
+
+function createElementWithTeamDescription({
+  strDescriptionEN,
+  strDescriptionPT,
+}) {
+  const p = document.createElement("p");
+  const description = strDescriptionPT ? strDescriptionPT : strDescriptionEN;
+  p.innerText = `${description}`;
+  teamDescription.appendChild(p);
+}
+
+function createElementWithGallery(...props) {
+  console.log(props);
+  let first = true;
+  for (const iterator of props) {
+    if (iterator) {
+      const img = document.createElement("img");
+      const div = document.createElement("div");
+      first
+        ? div.setAttribute("class", "carousel-item active")
+        : div.setAttribute("class", "carousel-item");
+      first = false;
+      img.setAttribute("src", iterator);
+      div.appendChild(img);
+      teamTeamGalery.appendChild(div);
+    }
+  }
+}
+
+function createElementWithSocialNetworks({
+  strYoutube,
+  strFacebook,
+  strInstagram,
+  strTwitter,
+}) {
+  strYoutube && teamTeamNetwork.appendChild(createLink(strYoutube, "youtube"));
+  strFacebook &&
+    teamTeamNetwork.appendChild(createLink(strFacebook, "Facebook"));
+  strInstagram &&
+    teamTeamNetwork.appendChild(createLink(strInstagram, "Instagram"));
+  strTwitter && teamTeamNetwork.appendChild(createLink(strTwitter, "twitter"));
+
+  function createLink(url, classe) {
+    const link = document.createElement("a");
+    link.setAttribute("href", `https://${url}`);
+    link.setAttribute("class", classe);
+    link.setAttribute("target", "_blank");
+    link.innerHTML =
+      classe === "youtube"
+        ? `<img src="https://img.icons8.com/ios/50/000000/youtube-squared.png"/>`
+        : classe === "Facebook"
+        ? `<img src="https://img.icons8.com/ios/50/000000/facebook--v1.png"/>`
+        : classe === "Instagram"
+        ? `<img src="https://img.icons8.com/ios/50/000000/instagram-new--v1.png"/>`
+        : `<img src="https://img.icons8.com/ios/50/000000/twitter-squared.png"/>`;
+    return link;
+  }
+}
+
+searchInfoTeam();
